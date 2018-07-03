e725274f4800da6a2d3275ff993ce316ddcde74d
Shivraj Rath <shivrajrath@gmail.com>
2018-07-02 19:24:51 -0700

    Initial commit of a working app

---
+++ README.md
@@ -0,0 +1,3 @@
+# JSON Sort Diff
+
+JSON comparison tool, which first sorts the JSON object and then compares them
---
+++ main.js
@@ -0,0 +1,68 @@
+/*!
+  Form / application "onsubmit" handler, and analytics.
+*/
+
+var jsonabc = require('jsonabc');
+var JsDiff = require('diff');
+
+window.appSort = appSort;
+
+// displaying the difference
+function diffDisplay(str1, str2) {
+  var diff = JsDiff.diffChars(str1, str2),
+    display = document.getElementById('diff'),
+    fragment = document.createDocumentFragment();
+
+  diff.forEach(function (part) {
+    // green for additions, red for deletions
+    // grey for common parts
+    color = part.added ? 'green' :
+      part.removed ? 'red' : 'grey';
+    span = document.createElement('span');
+    span.style.color = color;
+    span.appendChild(document
+      .createTextNode(part.value));
+    fragment.appendChild(span);
+  });
+
+  display.appendChild(fragment);
+}
+
+// app sorting
+function appSort(ev, tid1, tid2) {
+  ev.preventDefault();
+
+  var inputStr1, inputStr2, sortStr1, sortStr2;
+
+  inputStr1 = document.getElementById(tid1).value;
+  inputStr2 = document.getElementById(tid2).value;
+
+  try {
+    sortStr1 = jsonabc.sort(inputStr1);
+    sortStr2 = jsonabc.sort(inputStr2);
+
+    document.getElementById(tid1).value = sortStr1;
+    document.getElementById(tid2).value = sortStr2;
+
+    diffDisplay(sortStr1, sortStr2);
+  } catch (ex) {
+    window.alert('Incorrect JSON object');
+  }
+}
+
+/* eslint-disable */
+(function (i, s, o, g, r, a, m) {
+  i['GoogleAnalyticsObject'] = r;
+  i[r] = i[r] || function () {
+    (i[r].q = i[r].q || []).push(arguments)
+  }, i[r].l = 1 * new Date();
+  a = s.createElement(o),
+    m = s.getElementsByTagName(o)[0];
+  a.async = 1;
+  a.src = g;
+  m.parentNode.insertBefore(a, m)
+})(window, document, 'script', 'https://www.google-analytics.com/analytics.js', 'ga');
+
+ga('create', 'UA-58536835-1', 'auto');
+ga('send', 'pageview');
+/* eslint-enable */
